b71cb59b3a42d41e5572a8fa05e9a3cf49f50f37
Charron Maxime <maksouw85@gmail.com>
2025-06-27 10:01:26 +0200

    fix: revert bad changes

--- src/LelManga/LelManga.ts
+++ src/LelManga/LelManga.ts
@@ -11,14 +11,14 @@ import {
     MangaStream
 } from '../MangaStream'
 
-const DOMAIN = 'https://starboundscans.com'
+const DOMAIN = 'https://www.lelmanga.com'
 
 export const LelMangaInfo: SourceInfo = {
     version: getExportVersion('0.0.0'),
-    name: 'StarboundScans',
+    name: 'LelManga',
     description: `Extension that pulls manga from ${DOMAIN}`,
-    author: 'MaksOuw',
-    authorWebsite: 'http://github.com/MaksOuw',
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
     icon: 'icon.png',
     contentRating: ContentRating.MATURE,
     websiteBaseURL: DOMAIN,
@@ -35,7 +35,10 @@ export class LelManga extends MangaStream {
 
     baseUrl: string = DOMAIN
     override language = '🇫🇷'
-    
+
+    override manga_selector_author = 'Autheur'
+    override manga_selector_artist = 'Artiste'
+
     override configureSections() {
         this.homescreen_sections['popular_today'].selectorFunc = ($: CheerioAPI) => $('div.bsx', $('h2:contains(Top Managa Aujourd\'hui)')?.parent()?.next())
         this.homescreen_sections['latest_update'].selectorFunc = ($: CheerioAPI) => $('div.uta', $('h2:contains(Dernières Sorties)')?.parent()?.next())
