02d5af38d6510e384ac1bda5c595a20a39bc4465
MaksOuw <maksouw85@gmail.com>
2025-08-29 21:31:55 +0000

    Deploying to gh-pages from @ MaksOuw/extensions-generic-0.8@42ac301a395d80dc2142b8a39a84a6450855c1c7 🚀

--- 0.8/PhenixScans/source.js
+++ 0.8/PhenixScans/source.js
@@ -19764,7 +19764,7 @@ var _Sources = (() => {
     }
   };
 
-  // src/UrlBuilder.ts
+  // src/PhenixScans/UrlBuilder.ts
   var URLBuilder = class {
     constructor(baseUrl) {
       this.parameters = {};
@@ -19814,7 +19814,7 @@ var _Sources = (() => {
   // src/PhenixScans/PhenixScans.ts
   var DOMAIN = "https://phenix-scans.com";
   var PhenixScansInfo = {
-    version: "1.0.1",
+    version: "1.0.2",
     name: "PhenixScans",
     description: `Extension that pulls webtoons from ${DOMAIN}`,
     author: "MaksOuw",
