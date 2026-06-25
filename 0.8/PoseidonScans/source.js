3d207a06465add5ca71d9831d4e9383a65384661
MaksOuw <maksouw85@gmail.com>
2026-06-25 12:42:36 +0000

    Deploying to gh-pages from @ MaksOuw/extensions-generic-0.8@afd31ed9cf10e14aeed33f9dd0784875aca88069 🚀

--- 0.8/PoseidonScans/source.js
+++ 0.8/PoseidonScans/source.js
@@ -19418,7 +19418,7 @@ var _Sources = (() => {
   // src/PoseidonScans/PoseidonScans.ts
   var DOMAIN = "https://poseidon-scans.net";
   var PoseidonScansInfo = {
-    version: "1.1.2",
+    version: "1.1.3",
     name: "PoseidonScans",
     description: `Extension that pulls webtoons from ${DOMAIN}`,
     author: "MaksOuw",
