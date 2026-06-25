51f3deb8f0618861ad9743533eae36358ae04a08
Charron Maxime <maksouw85@gmail.com>
2026-06-25 14:41:40 +0200

    fix: erase user-agent to fix cloudflare

--- src/PoseidonScans/PoseidonScans.ts
+++ src/PoseidonScans/PoseidonScans.ts
@@ -126,7 +126,7 @@ export class PoseidonScans implements ChapterProviding, HomePageSectionsProvidin
             url: `${this.baseUrl}/${this.directoryPath}/${mangaId}/`,
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+                'User-Agent': await this.requestManager.getDefaultUserAgent(),
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language': 'fr-FR,fr;q=0.9',
             }
@@ -144,7 +144,7 @@ export class PoseidonScans implements ChapterProviding, HomePageSectionsProvidin
             url: `${this.baseUrl}/${this.directoryPath}/${mangaId}/chapter/${chapterId}`,
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+                'User-Agent': await this.requestManager.getDefaultUserAgent(),
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language': 'fr-FR,fr;q=0.9',
             }
@@ -162,7 +162,7 @@ export class PoseidonScans implements ChapterProviding, HomePageSectionsProvidin
             url: `${this.baseUrl}/${this.directoryPath}/${mangaId}/`,
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+                'User-Agent': await this.requestManager.getDefaultUserAgent(),
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language': 'fr-FR,fr;q=0.9',
             }
@@ -184,7 +184,7 @@ export class PoseidonScans implements ChapterProviding, HomePageSectionsProvidin
             url: `${this.baseUrl}/`,
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+                'User-Agent': await this.requestManager.getDefaultUserAgent(),
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language': 'fr-FR,fr;q=0.9',
             }
